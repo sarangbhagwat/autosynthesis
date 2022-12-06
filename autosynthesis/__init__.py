ca306ce915bf1b8a67a99513c5752d4b16f9a1c0
sarangbhagwat <59667492+sarangbhagwat@users.noreply.github.com>
2022-12-06 15:49:56 -0600

    v 0.0.12

--- autosynthesis/__init__.py
+++ autosynthesis/__init__.py
@@ -6,7 +6,7 @@
 # https://github.com/sarangbhagwat/autosynthesis/blob/main/LICENSE
 # for license details.
 
-__version__ = '0.0.11'
+__version__ = '0.0.12'
 __author__ = 'Sarang S. Bhagwat'
 
 # %% Initialize AutoSynthesis
