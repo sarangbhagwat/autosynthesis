3333bedc3b15047202d2f38927eb2ff5b409a3ab
sarangbhagwat <59667492+sarangbhagwat@users.noreply.github.com>
2022-12-30 21:15:36 -0600

    v 0.0.15

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 setup(
     name='autosynthesis',
     packages=['autosynthesis'],
-    version='0.0.14',    
+    version='0.0.15',    
     description='A toolkit for automated synthesis of biorefinery processes including pretreatment, conversion, separation, and upgrading.',
     url='https://github.com/sarangbhagwat/autosynthesis',
     author='Sarang S. Bhagwat',
