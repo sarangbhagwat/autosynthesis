f97cac0241b77b81a0d74d308b3cbe8a5f763c52
sarangbhagwat <sarangb2@illinois.edu>
2025-06-25 13:02:36 -0700

    v 0.0.24

--- autosynthesis/__init__.py
+++ autosynthesis/__init__.py
@@ -6,7 +6,7 @@
 # https://github.com/sarangbhagwat/autosynthesis/blob/main/LICENSE
 # for license details.
 
-__version__ = '0.0.23'
+__version__ = '0.0.24'
 __author__ = 'Sarang S. Bhagwat'
 
 #%% Initialize AutoSynthesis
