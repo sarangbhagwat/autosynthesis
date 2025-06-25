8b7e37a8f9a327fa3a203cb5f2c8c5ca7ea26496
sarangbhagwat <sarangb2@illinois.edu>
2025-06-25 14:44:46 -0700

    cleanup and minor edits

--- setup.py
+++ setup.py
@@ -13,20 +13,28 @@ setup(
     version='0.0.26',    
     description='A toolkit for automated synthesis of biorefinery processes including pretreatment, conversion, separation, and upgrading.',
     url='https://github.com/sarangbhagwat/autosynthesis',
+    download_url='https://github.com/sarangbhagwat/autosynthesis.git',
     author='Sarang S. Bhagwat',
-    author_email='sarang.bhagwat.git@gmail.com',
+    author_email='sarangbhagwat.developer@gmail.com',
     license='MIT',
     # packages=['contourplots'],
+    python_requires='>=3.9',
+    platforms=['Windows', 'Mac', 'Linux'],
     install_requires=[
                       'biosteam>=2.33.2',
                       'xlsxwriter',
                       ],
-
+    package_dir=['autosynthesis'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: University of Illinois/NCSA Open Source License',  
-        'Operating System :: Microsoft :: Windows',        
-        'Programming Language :: Python :: 3.8'
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: BSD',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.9',
     ],
 )
